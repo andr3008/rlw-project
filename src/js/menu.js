cb3019a08dce35329910dcddfc89177c1ea253bb
Andrei Potapov <andr3008@gmail.com>
2021-09-29 15:56:39 +0300

    change

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,12 +3,14 @@
     openMenuBtn: document.querySelector('.menu__open'),
     closeMenuBtn: document.querySelector('.menu__close'),
     menu: document.querySelector('.menu__container'),
+    mobileMenu: document.querySelector('.mobile__menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
+  refs.mobileMenu.addEventListener('click', toggleMenu);
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.mobileMenu.classList.toggle('is-hidden');
   }
 })();
